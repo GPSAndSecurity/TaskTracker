18e653aeda4ba640ef899eb3593e25f18f4435a4
Paola <pmunoz@unah.hn>
2025-08-21 11:07:46 -0600

    proyect form
    delete the option add user when create the project

--- obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74755ad16a01fedbd52cd84c3e6f737db3e11e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa6b915030b525cb2ebf6b35024c746e937ab5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
