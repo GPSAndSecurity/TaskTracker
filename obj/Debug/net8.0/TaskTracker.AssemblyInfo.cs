2d0d9134e1850d84c9ff533b84a293e6ab8677c3
Paola <pmunoz@unah.hn>
2025-08-23 11:55:54 -0600

    x
    xsd

--- obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71ba978aabdc5d3881ef6c2def999a8ab090f12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e12b9bd49abf7e402ea4a0c57b1f095fd74a22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
